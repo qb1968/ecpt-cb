b112d0aa8dc959931823a14eee6c4f46a14f49c1
Jonathan Mark Allison <jallison@nexient.com>
2023-01-10 18:47:10 -0500

    18

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,9 +15,9 @@ const Home = () => {
         </div>
         <div className="p1">    
             <p className="elite">
-                <h5>Elite Care Physical Therapy at Chesapeake Beach was founded in 2017 and serves the Anne Arundel/Calvert County area. Winner of multiple patient-nominated awards for best Physical Therapy office in Calvert County 3 years in a row, we treat a variety of orthopedic and neurological conditions. We invite you to see what makes our office unique and why</h5>
-                <h5  className="special" style={{fontWeight:800,fontStyle:"italic",}}>"It's not just Physical Therapy, its Elite Care"</h5>
-           
+                <h5>Elite Care Physical Therapy at Chesapeake Beach was founded in 2017 and serves the Anne Arundel/Calvert County area. Winner of multiple patient-nominated awards for best Physical Therapy office in Calvert County 3 years in a row, we treat a variety of orthopedic and neurological conditions. We invite you to see what makes our office unique and why </h5>
+                <h5  className="special" style={{fontWeight:800,fontStyle:"italic",}}>"It's not just Physical Therapy, </h5>
+                <h5 className="special1" style={{fontWeight:800,fontStyle:"italic",}}>its Elite Care"</h5>
             </p>
              <img class="sign" src={myImage} alt="Logo"></img>
         </div>
