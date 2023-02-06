5dba00f0096d043798e3a9d8f5e7af4e1e79dd82
Jonathan Mark Allison <jallison@nexient.com>
2023-02-06 18:51:47 -0500

    33

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Bio from '../assets/charles-sclater.jpg'
+import Bio from '../assets/charlie2.jpg'
 
 const About = () => {
     return (
@@ -7,7 +7,7 @@ const About = () => {
             
                <h1 >Charles Sclater, MPT, GTS</h1> 
                <div className='about1'>
-               <p><h4 style={{fontStyle:"oblique"}}>Charlie graduated from the University of Maryland Eastern Shore with his Masters in Physical Therapy Degree in 1997. Since then he has been a practicing physical therapist in 3 states and multiple settings including acute care, skilled nursing, rehabilitation centers, and his favorite, out-patient orthopedics. He has taken extensive continuing education courses in conservative and post-surgical shoulder rehabilitation, knee joint and ACL injury rehabilitation, and evaluation and treatment of temporo-mandibular dysfunction.
+               <p ><h4 className='cs'style={{fontStyle:"oblique"}}>Charlie graduated from the University of Maryland Eastern Shore with his Masters in Physical Therapy Degree in 1997. Since then he has been a practicing physical therapist in 3 states and multiple settings including acute care, skilled nursing, rehabilitation centers, and his favorite, out-patient orthopedics. He has taken extensive continuing education courses in conservative and post-surgical shoulder rehabilitation, knee joint and ACL injury rehabilitation, and evaluation and treatment of temporo-mandibular dysfunction.
 
                    Charlie opened and was the sole owner of Bayview Physical Therapy in Chesapeake Beach from 2011-2014. He recently joined Elite Care Physical Therapy in 2017.
 
